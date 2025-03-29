775c62a1a6799062c6b48866fbd78a38c328360f
James Medley <jamesmedley00@gmail.com>
2025-03-29 02:27:55 +0000

    F4 results

--- pkl_to_plots.py
+++ pkl_to_plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from skopt import load
 from skopt.plots import plot_convergence, plot_objective, plot_evaluations
 
-result = load("hp_optim_results_F3.pkl")
+result = load("hp_optim_results_F4.pkl")
 
 # Best hyperparameters
 best_hyperparameters = {

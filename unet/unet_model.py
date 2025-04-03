fc1d639c0537f4e38f6f1e15dd64ed1b1e882e49
James Medley <jamesmedley00@gmail.com>
2025-04-03 18:32:32 +0100

    DUCK-Net implementation

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -1,48 +1,179 @@
-""" Full assembly of the parts to form the complete network """
+"""
+Paper:      Using DUCK-Net for Polyp Image Segmentation
+Url:        https://arxiv.org/abs/2311.02239
+Create by:  zh320
+Date:       2024/11/08
+"""
 
-from .unet_parts import *
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .modules import conv1x1, ConvBNAct, Activation
 
 
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
+class UNet(nn.Module):  # DUCK-Net implementation, ignore class name
+    def __init__(self, num_class=1, n_channel=3, base_channel=17, act_type='relu'):
+        super().__init__()
+        self.down_stage1 = DownsampleBlock(n_channel, base_channel*2, act_type, fuse_channels=base_channel)
+        self.down_stage2 = DownsampleBlock(base_channel*2, base_channel*4, act_type)
+        self.down_stage3 = DownsampleBlock(base_channel*4, base_channel*8, act_type)
+        self.down_stage4 = DownsampleBlock(base_channel*8, base_channel*16, act_type)
+        self.down_stage5 = DownsampleBlock(base_channel*16, base_channel*32, act_type)
+        self.mid_stage = nn.Sequential(
+                            ResidualBlock(base_channel*32, base_channel*32, act_type),
+                            ResidualBlock(base_channel*32, base_channel*32, act_type),
+                            ResidualBlock(base_channel*32, base_channel*16, act_type),
+                            ResidualBlock(base_channel*16, base_channel*16, act_type),
+                        )
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.up_stage5 = UpsampleBlock(base_channel*16, base_channel*8, act_type)
+        self.up_stage4 = UpsampleBlock(base_channel*8, base_channel*4, act_type)
+        self.up_stage3 = UpsampleBlock(base_channel*4, base_channel*2, act_type)
+        self.up_stage2 = UpsampleBlock(base_channel*2, base_channel, act_type)
+        self.up_stage1 = UpsampleBlock(base_channel, base_channel, act_type)
+        self.seg_head = conv1x1(base_channel, num_class)
 
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        x1, x1_skip, x = self.down_stage1(x)
+        x2, x2_skip, x = self.down_stage2(x1+x, x)
+        x3, x3_skip, x = self.down_stage3(x2+x, x)
+        x4, x4_skip, x = self.down_stage4(x3+x, x)
+        x5, x5_skip, x = self.down_stage5(x4+x, x)
+        x = self.mid_stage(x5+x)
 
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+        x = self.up_stage5(x, x5_skip)
+        x = self.up_stage4(x, x4_skip)
+        x = self.up_stage3(x, x3_skip)
+        x = self.up_stage2(x, x2_skip)
+        x = self.up_stage1(x, x1_skip)
+        x = self.seg_head(x)
+
+        return x
+
+
+class DownsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, act_type, fuse_channels=None):
+        super().__init__()
+        fuse_channels = in_channels if fuse_channels is None else fuse_channels
+        self.duck = DUCK(in_channels, fuse_channels, act_type)
+        self.conv1 = ConvBNAct(fuse_channels, out_channels, 3, 2, act_type=act_type)
+        self.conv2 = ConvBNAct(in_channels, out_channels, 2, 2, act_type=act_type)
+
+    def forward(self, x1, x2=None):
+        if x2 is None:
+            x2 = self.conv2(x1)
+        else:
+            x2 = self.conv2(x2)
+
+        skip = self.duck(x1)
+        x1 = self.conv1(skip)
+
+        return x1, skip, x2
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, act_type):
+        super().__init__()
+        self.duck = DUCK(in_channels, out_channels, act_type)
+
+    def forward(self, x, residual):
+        size = residual.size()[2:]
+        x = F.interpolate(x, size, mode='nearest')
+
+        x += residual
+        x = self.duck(x)
+
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, act_type):
+        super().__init__()
+        self.upper_branch = conv1x1(in_channels, out_channels)
+        self.lower_branch = nn.Sequential(
+                                ConvBNAct(in_channels, out_channels, 3, act_type=act_type),
+                                ConvBNAct(out_channels, out_channels, 3, act_type=act_type),
+                            )
+        self.bn = nn.Sequential(
+                        nn.BatchNorm2d(out_channels),
+                        Activation(act_type)
+                    )
+
+    def forward(self, x):
+        x_up = self.upper_branch(x)
+        x_low = self.lower_branch(x)
+
+        x = x_up + x_low
+        x = self.bn(x)
+
+        return x
+
+
+class DUCK(nn.Module):
+    def __init__(self, in_channels, out_channels, act_type, filter_size=6+1):
+        '''
+        Here I change the filter size of separated block to be odd number.
+        '''
+        super().__init__()
+        self.in_bn = nn.Sequential(
+                        nn.BatchNorm2d(in_channels),
+                        Activation(act_type)
+                    )
+        self.branch1 = WidescopeBlock(in_channels, out_channels, act_type)
+        self.branch2 = MidscopeBlock(in_channels, out_channels, act_type)
+        self.branch3 = ResidualBlock(in_channels, out_channels, act_type)
+        self.branch4 = nn.Sequential(
+                            ResidualBlock(in_channels, out_channels, act_type),
+                            ResidualBlock(out_channels, out_channels, act_type),
+                        )
+        self.branch5 = nn.Sequential(
+                            ResidualBlock(in_channels, out_channels, act_type),
+                            ResidualBlock(out_channels, out_channels, act_type),
+                            ResidualBlock(out_channels, out_channels, act_type),
+                        )
+        self.branch6 = SeparatedBlock(in_channels, out_channels, filter_size, act_type)
+        self.out_bn = nn.Sequential(
+                        nn.BatchNorm2d(out_channels),
+                        Activation(act_type)
+                    )
+
+    def forward(self, x):
+        x = self.in_bn(x)
+
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        x3 = self.branch3(x)
+        x4 = self.branch4(x)
+        x5 = self.branch5(x)
+        x6 = self.branch6(x)
+
+        x = x1 + x2 + x3 + x4 + x5 + x6
+        x = self.out_bn(x)
+
+        return x
+
+
+class MidscopeBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels, act_type):
+        super().__init__(
+            ConvBNAct(in_channels, out_channels, 3, act_type=act_type),
+            ConvBNAct(out_channels, out_channels, 3, dilation=2, act_type=act_type)
+        )
+
+
+class WidescopeBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels, act_type):
+        super().__init__(
+            ConvBNAct(in_channels, out_channels, 3, act_type=act_type),
+            ConvBNAct(out_channels, out_channels, 3, dilation=2, act_type=act_type),
+            ConvBNAct(out_channels, out_channels, 3, dilation=3, act_type=act_type),
+        )
+
+
+class SeparatedBlock(nn.Sequential):
+    def __init__(self, in_channels, out_channels, filter_size, act_type):
+        super().__init__(
+            ConvBNAct(in_channels, out_channels, (1, filter_size), act_type=act_type),
+            ConvBNAct(out_channels, out_channels, (filter_size, 1), act_type=act_type),
+        )

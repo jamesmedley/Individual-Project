b1e6d14bcf2ccfa454eea14c70d17564a2b018b3
James Medley <jamesmedley00@gmail.com>
2025-04-11 16:09:17 +0100

    fix

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -11,10 +11,10 @@ class UNet(nn.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
         self.input_shape = (128, 128)
-        self.J = 3
+        self.J = 4
         self.L = 8
 
-        self.S = Scattering2D(J=self.J, shape=self.input_shape, L=self.L)
+        self.S = Scattering2D(J=self.J, shape=self.input_shape, L=self.L).cuda()
         n_order1 = n_channels * (1 + self.J * self.L)
         n_order2 = n_channels * ((self.L ** 2 * self.J * (self.J - 1)) // 2)
         n_input_channels = n_order1 + n_order2
@@ -25,15 +25,16 @@ class UNet(nn.Module):
         # removed down3
         factor = 2 if bilinear else 1
         # removed down4
-        self.up1 = (Up(1024, 512 // factor, bilinear, n_final_skip=n_order2))  # skip order 2 coeffs
+        self.up1 = (Up(1024, 512 // factor, bilinear))
         self.up2 = (Up(512, 256 // factor, bilinear))
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear, n_final_skip=67))  # skip: 64 + input channels
         self.outc = (OutConv(64, n_classes))
 
-        self.skip_upconv1 = (SkipTripleUpConv(n_order1, 64))
-        self.skip_upconv2 = (SkipDoubleUpConv(n_order1, 128))
-        self.skip_upconv3 = (SkipUpConv(n_order2, 256))
+        self.skip_upconv1 = (SkipFourUpConv(n_order1, 64))
+        self.skip_upconv2 = (SkipTripleUpConv(n_order1, 128))
+        self.skip_upconv3 = (SkipDoubleUpConv(n_order2, 256))
+        self.skip_upconv4 = (SkipUpConv(n_order2, 512))
 
     def forward(self, x):
         scattering_coeffs = self.S.scattering(x.contiguous())  # Shape: (B, C, scattering_channels, H', W')
@@ -52,15 +53,15 @@ class UNet(nn.Module):
         # Reshape for CNN layers: Merge scattering channels into the channel dimension but keep RGB separate
         coeffs_order1 = coeffs_order1.reshape(B, C * (n_order0 + n_order1), H, W)  # (B, C * (n_order0 + n_order1), H', W')
         coeffs_order2 = coeffs_order2.reshape(B, C * n_order2, H, W)  # (B, C * n_order2, H', W')
-
         skip_scat_coeffs_o1 = self.skip_upconv1(coeffs_order1)  # param: first n_order1 coefficients
-        skip_order_1 = torch.cat([x, skip_scat_coeffs_o1], dim=1)
 
+        skip_order_1 = torch.cat([x, skip_scat_coeffs_o1], dim=1)
         skip_order_2 = self.skip_upconv2(coeffs_order1)
         skip_order_3 = self.skip_upconv3(coeffs_order2)
+        skip_order_4 = self.skip_upconv4(coeffs_order2)
 
         x1 = self.inc(all_coeffs)
-        x = self.up1(x1, coeffs_order2)
+        x = self.up1(x1, skip_order_4)
         x = self.up2(x, skip_order_3)
         x = self.up3(x, skip_order_2)
         x = self.up4(x, skip_order_1)

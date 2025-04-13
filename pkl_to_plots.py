ae100e730af2dcad1fa4e8278ae4b117e70e9cc4
James Medley <jamesmedley00@gmail.com>
2025-04-13 10:26:03 +0100

    j2_skip all hp

--- pkl_to_plots.py
+++ pkl_to_plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from skopt import load
 from skopt.plots import plot_convergence, plot_objective, plot_evaluations
 
-result = load("hp_optim_results_F10.4.pkl")
+result = load("hp_optim_results_J2(skip_all).pkl")
 
 # Best hyperparameters
 best_hyperparameters = {

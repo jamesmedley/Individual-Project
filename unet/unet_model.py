09c5fa5261ba2d1efec0aea0423b4167f510378b
James Medley <jamesmedley00@gmail.com>
2025-05-01 04:27:11 +0100

    retinal demo masks

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -10,7 +10,7 @@ class UNet(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
-        self.input_shape = (128, 128)
+        self.input_shape = (584, 564)
         J = 1
         L = 16
 
